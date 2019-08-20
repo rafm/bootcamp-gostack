bb66445e47be6a538dff5c6b82d08fbe93247503
Rafael Maia <rafael.augusto.fm@outlook.com>
2019-08-20 23:59:25 +0000

    Creating middleware to count and print the number of requests received.

--- desafio-01/index.js
+++ desafio-01/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+let requestCount = 0;
 
 express()
     .use(express.json())
+    .use((request, response, next) => {
+        console.log(`Request number: ${++requestCount}`);
+        return next();
+    })
     .use(require('./src/routes'))
     .listen(3000);
