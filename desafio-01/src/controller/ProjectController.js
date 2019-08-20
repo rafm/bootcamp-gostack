22910bd805253f78e0b63eefbc24dbcbe563c36e
Rafael Maia <rafael.augusto.fm@outlook.com>
2019-08-20 22:25:49 +0000

    Creating endpoint to find a project by id.

--- desafio-01/src/controller/ProjectController.js
+++ desafio-01/src/controller/ProjectController.js
@@ -1,18 +1,33 @@
 const projects = [];
 
+function _findById(id) {
+    return projects.filter(project => project.id === id)[0] || null;
+}
+
 module.exports = {
 
     findAll(request, response) {
         return response.json(projects);
     },
 
+    findById(request, response) {
+        const { id } = request.params;
+
+        const project = _findById(id);
+        if (!project) {
+            return response.status(404).send(`Project with id ${id} was not found.`)
+        }
+
+        return response.json(project);
+    },
+
     create(request, response) {
         const project = request.body;
 
-        if (projects.filter(prj => prj.id === project.id).length != 0) {
+        if (_findById(project.id)) {
             return response.status(422).send(`Project with the id ${project.id} already exists.`);
         }
-        
+
         projects.push(project);
 
         return response.status(201).json(project);
--- desafio-01/src/routes.js
+++ desafio-01/src/routes.js
@@ -4,6 +4,7 @@ const ProjectController = require('./controller/ProjectController');
 
 router
     .get('/projects', ProjectController.findAll)
+    .get('/projects/:id', ProjectController.findById)
     .post('/projects', ProjectController.create);
 
 module.exports = router;
