be356da77ac499ce8742de7a1a6069e051b576c9
Rafael Maia <rafael.augusto.fm@outlook.com>
2019-10-28 19:33:59 +0000

    02-gobarber: mapping the app model classes' with the Sequelize.

--- 02-iniciando-back-end-do-gobarber/src/app.js
+++ 02-iniciando-back-end-do-gobarber/src/app.js
@@ -1,6 +1,11 @@
 import express from 'express'; // const express = require('express');
 import routes from './routes'; // const routes = require('./routes');
 
+import './database';    // When you won't gonna store the import value, you don't need to use the from keyword,
+                        // in this case, we just need to run the models mapping logic.
+                        // Also, want to import a file/js which its name is "index.js", you don't need to pass
+                        // the file name, only the package name.
+
 class App {
     constructor() {
         this.server = express();
