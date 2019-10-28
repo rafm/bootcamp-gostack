d7022d04ad691832c5574fbedc0b12511c8ab597
Rafael Maia <rafael.augusto.fm@outlook.com>
2019-10-28 20:45:27 +0000

    02-gobarber: creating endpoint to create an user.

--- 02-iniciando-back-end-do-gobarber/src/routes.js
+++ 02-iniciando-back-end-do-gobarber/src/routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController'
 
 // Router() = new Router()
 // call constructor/function and returns an object with the defined functions and attributes of the class
 const routes = new Router();
-routes.get('/', (request, response) => response.send('Hello world!'));
+routes.post('/users', UserController.store);
 
 export default routes;
