aa81c5e09f65dbd41c69262ba0edb25bb31434a0
Rafael Maia <rafael.augusto.fm@outlook.com>
2019-10-30 20:21:43 +0000

    02-desafio: creating endpoint to create a Student.

--- 02-desafio/src/app/controllers/StudentController.js
+++ 02-desafio/src/app/controllers/StudentController.js
@@ -1,6 +1,43 @@
+import * as Yup from 'yup';
+import Student from '../models/Student';
+
 class StudentController {
     async store(request, response) {
-        return response.send(`Student POST, userId: ${request.userId}`);
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string()
+                .required()
+                .email(),
+            age: Yup.number()
+                .integer()
+                .min(2)
+                .max(150),
+            weight: Yup.number()
+                .min(1)
+                .max(999),
+            height: Yup.number()
+                .integer()
+                .min(10)
+                .max(350),
+        });
+
+        if (!(await schema.isValid(request.body))) {
+            return response.status(400).json({ error: 'Validation fails' });
+        }
+
+        const studentExists = await Student.findOne({
+            where: { email: request.body.email },
+        });
+        if (studentExists) {
+            return response
+                .status(400)
+                .json({ error: 'Student already exists' });
+        }
+
+        const { id, name, email, age, weight, height } = await Student.create(
+            request.body
+        );
+        return response.json({ id, name, email, age, weight, height });
     }
 
     async update(request, response) {
