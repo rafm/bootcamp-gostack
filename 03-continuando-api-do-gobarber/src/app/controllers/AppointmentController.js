3781515e280f1f855d6f7eee3b5a70517adcbf33
Rafael Maia <rafael.augusto.fm@outlook.com>
2019-11-07 05:20:15 +0000

    03-gobarber: creating notification in mongodb for user provider after creating an appointment.

--- 03-continuando-api-do-gobarber/src/app/controllers/AppointmentController.js
+++ 03-continuando-api-do-gobarber/src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
     async index(request, response) {
@@ -95,6 +97,20 @@ class AppointmentController {
             date: hourStart,
             user_id: request.userId,
             provider_id,
+        });
+
+        /**
+         * Notify appointment provider
+         */
+        const user = await User.findByPk(request.userId);
+        const formattedDate = format(
+            hourStart,
+            "'dia' dd 'de' MMMM', às' H:mm'h'",
+            { locale: pt }
+        );
+        await Notification.create({
+            content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+            user: provider_id,
         });
 
         return response.json(appointment);
